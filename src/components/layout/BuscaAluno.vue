0eb2fc505f5d301872c71ec837328e5e28852315
Leonardo <leonardo@visie.com.br>
2019-05-27 15:04:37 -0300

    configurando consulta ao banco

---
+++ src/components/layout/BuscaAluno.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>Lista de Alunos</h1>
+</template>
+
+<script>
+export default {
+  name: "Lista",
+  data: () => ({
+  alunoss: []
+  }),
+  created() {
+    this.getData();
+  },
+
+  methods: {
+    getData() {
+      this.$firebase
+        .firestore()
+        .doc("alunos/ListaGeral")
+        .get()
+        .then(function(doc) {
+          if (doc.exists) {
+            console.log("Nome :"+doc.data().nome + "  Idade: "+doc.data().idade);
+
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        })
+        .catch(function(error) {
+          console.log("Error getting document:", error);
+        });
+
+    }
+  }
+};
+</script>
+
+<style>
+</style>
