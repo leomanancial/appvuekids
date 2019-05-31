dec17eb7857373022222e8c6f54581e09d013445
Leonardo <leonardo@visie.com.br>
2019-05-31 00:23:27 -0300

    criando banco e logica de lista de alunos

--- src/pages/novo-aluno/Novo-aluno.vue
+++ src/pages/novo-aluno/Novo-aluno.vue
@@ -1,41 +1,67 @@
 <template>
   <form>
     <h1>Novo Aluno</h1>
-    <hr />
+    <hr>
+
     <div class="container-fluid">
       <picture class="form-group mx-sm-3 mb-3">
         <div>
-          <img src="/public/logo_kids.png">
+          <img alt="Dashboard" src="/src/pages/novo-aluno/avatar.png" title="Dashboard">
         </div>
       </picture>
-
       <div class="form-group row">
-        <div class="col-10">
-          <input class="form-control" type="text" placeholder="Nome" id="nome-input">
+        <div class="col-2">
+          <input
+            class="form-control"
+            type="text"
+            placeholder
+            id="codigo-input"
+            disabled
+            v-model="form.codigo"
+          >
         </div>
-      </div>
-      <div class="form-group row">
-        <div class="col-10">
+        <div class="col-6">
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Nome da criança"
+            id="nome-input"
+            v-model="form.nome"
+          >
+        </div>
+
+        <div class="col-4">
           <input
             class="form-control"
             type="date"
             placeholder="Data Nascimento"
             id="nascimento-input"
+            v-model="form.nascimento"
           >
         </div>
       </div>
       <div class="form-group row">
-        <div class="col-10">
-          <input class="form-control" type="text" placeholder="Responsável" id="resp-input">
+        <div class="col-5">
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Pais ou Responsável"
+            id="resp-input"
+            v-model="form.resp"
+          >
         </div>
-      </div>
-      <div class="form-group row">
-        <div class="col-10">
-          <input class="form-control" type="text" placeholder="Telefone" id="telefone-input">
+
+        <div class="col-4">
+          <input
+            class="form-control"
+            type="tel"
+            placeholder="Telefone"
+            id="telefone-input"
+            v-model="form.tel"
+          >
         </div>
-      </div>
-      <div class="form-group row">
-        <div class="col-10">
+
+        <div class="col-3">
           <select class="custom-select" id="inlineFormCustomSelect">
             <option selected>Sala</option>
             <option value="1">Amarela</option>
@@ -47,7 +73,7 @@
     </div>
 
     <div class="form-inline mx-sm-3">
-      <button type="button" class="btn btn-success mb-4">
+      <button type="submit" class="btn btn-success mb-4">
         <i class="fas fa-user-plus"></i> Salvar
       </button>
       <div>
@@ -60,7 +86,60 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "NovoAluno",
+  data: () => ({
+    form: {
+      codigo: "",
+      nome: "",
+      nascimento: "",
+      resp: "",
+      tel: "",
+      sala: ""
+    }
+  }),
+
+  created() {
+    var size = 2; //qtde de digitos
+    var randomized = Math.ceil(Math.random() * Math.pow(10, size));
+    var digito = Math.ceil(Math.log(randomized));
+    while (digito > 10) {
+      digito = Math.ceil(Math.log(digito));
+    }
+    var rId = randomized + digito;
+
+    const ref = this.$firebase.database().ref("ListaAlunos");
+    //const id = ref.push().key;
+    const id = rId;
+
+    /*  const id = rId;
+    const ref = this.$firebase.firestore();
+  */
+
+    const registro = {
+      id,
+      dt_registro: new Date().getDate(),
+      aluno: "Mauro",
+      dt_nasc: "12/12/2312",
+      responsavel: "Me",
+      sala: "Amarela"
+    };
+
+    ref.child(id).set(registro);
+    /*
+    ref
+      .collection("Lista")
+      .doc("test")
+      .set(registro)
+      .then(function() {
+        console.log("Document successfully written!");
+      }); */
+  },
+
+  methods: {
+    submit() {}
+  }
+};
 </script>
 
 <style>
