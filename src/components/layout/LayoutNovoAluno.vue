60f52a7c254d916483c816d6bc5e01e826667653
Leonardo <leonardo@visie.com.br>
2019-06-02 02:02:35 -0300

    finalizado upload de foto

--- src/components/layout/LayoutNovoAluno.vue
+++ src/components/layout/LayoutNovoAluno.vue
@@ -13,6 +13,9 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
+            <div class="form-group">
+              <img v-bind:src="myPic" style="border:none">
+            </div>
             <h1 class="modal-title">Novo Aluno</h1>
             <button
               type="button"
@@ -24,18 +27,78 @@
               <span>×</span>
             </button>
           </div>
+
           <div class="modal-body">
-            <img v-bind:src="myPic" style="border:none">
-            <input type="text" placeholder="Nome completo" v-model="form.nome" required>
-            <input type="datetime" v-model="form.nascimento" required>
-            <input type="text" placeholder="Pais ou Responsável" v-model="form.resp" required>
-            <input type="tel" placeholder="Tel de contato" v-model="form.tel" required>
-            <select class="custom-select" v-model="form.sala" required>
-              <option selected>Sala</option>
-              <option value="Amarela">Amarela</option>
-              <option value="Verde">Verde</option>
-              <option value="Azul">Azul</option>
-            </select>
+            <div class="row">
+              <div class="form-group col-8">
+                <div v-if="form.foto">
+                  {{form.foto.name}}
+                  <button @click="form.foto = ''" class="btn badge badge-light">
+                    <i class="fa fa-trash text-danger"></i>
+                  </button>
+                </div>
+
+                <input
+                  ref="input"
+                  type="file"
+                  class="d-none"
+                  accept="image/*"
+                  @change="handleFile($event)"
+                >
+                <button
+                  @click="openFileDialog"
+                  type="button"
+                  class="btn btn-outline-secondary"
+                >Enviar Foto</button>
+              </div>
+
+              <div class="form-group col-8">
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="Nome"
+                  v-model="form.nome"
+                  required
+                >
+                <small id="emailHelp" class="form-text text-muted">Nome da criança completo</small>
+              </div>
+              <div class="form-group col-8">
+                <input class="form-control" type="date" v-model="form.nascimento" required>
+                <small id="emailHelp" class="form-text text-muted">Data de nascimento</small>
+              </div>
+              <div class="form-group col-8">
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="Responsável"
+                  v-model="form.resp"
+                  required
+                >
+                <small
+                  id="emailHelp"
+                  class="form-text text-muted"
+                >Nome dos pais ou responsável pela criança</small>
+              </div>
+              <div class="form-group col-8">
+                <input
+                  class="form-control"
+                  type="tel"
+                  placeholder="Tel de contato"
+                  v-model="form.tel"
+                  required
+                >
+                <small id="emailHelp" class="form-text text-muted">Telefone para contato</small>
+              </div>
+              <div class="form-group col-8">
+                <select class="custom-select" v-model="form.sala" required>
+                  <option selected>Sala</option>
+                  <option value="Amarela">Amarela</option>
+                  <option value="Verde">Verde</option>
+                  <option value="Azul">Azul</option>
+                </select>
+                <small id="emailHelp" class="form-text text-muted">Sala que a criança está</small>
+              </div>
+            </div>
           </div>
 
           <div class="modal-footer">
@@ -54,13 +117,16 @@
 </template>
 
 <script>
-import Foto from "../../static/avatar.png";
+import LogoKids from "../../static/avatar.png";
 export default {
   data: () => ({
     showModal: false,
-    myPic: Foto,
+    myPic: LogoKids,
+    value: "",
+    url: "",
     form: {
       id: "",
+      foto: "",
       nome: "",
       nascimento: "",
       resp: "",
@@ -69,11 +135,19 @@ export default {
     }
   }),
   methods: {
+    openFileDialog() {
+      this.$refs.input.value = null;
+      this.$refs.input.click();
+    },
+    handleFile({ target }) {
+      this.form.foto = target.files[0];
+    },
+
     closeModal() {
       this.showModal = false;
     },
 
-    submit() {
+    async submit() {
       this.$root.$emit("Spinner::show");
       const ref = this.$firebase.database().ref("ListaAlunos");
       //Gerador ID
@@ -86,6 +160,18 @@ export default {
         this.form.id = ref.push().key;
       } else {
         this.form.id;
+      }
+
+      if (this.form.foto) {
+        const snapshot = await this.$firebase
+          .storage()
+          .ref("FotoAlunos")
+          .child(this.form.id)
+          .put(this.form.foto);
+
+        const url = await snapshot.ref.getDownloadURL();
+
+        this.form.foto = url;
       }
 
       ref.child(this.form.id).update(this.form, err => {
