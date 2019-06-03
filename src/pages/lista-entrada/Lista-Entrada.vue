7921f2cafe39562f5b975279f459712755ee3b52
Leonardo <leonardo@visie.com.br>
2019-06-02 21:11:24 -0300

    finalizando listas

--- src/pages/lista-entrada/Lista-Entrada.vue
+++ src/pages/lista-entrada/Lista-Entrada.vue
@@ -1,5 +1,55 @@
 <template>
-
+  <div class="container-fluid">
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Código</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Responsável</th>
+          <th scope="col">Cartão</th>
+          <th scope="col"></th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+          <td>
+            <button class="btn btn-warning">Editar</button>
+          </td>
+          <td>
+            <button class="btn btn-danger">Excluir</button>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+          <td>
+            <button class="btn btn-warning">Editar</button>
+          </td>
+          <td>
+            <button class="btn btn-danger">Excluir</button>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td colspan="2">Larry the Bird</td>
+          <td>@twitter</td>
+          <td>
+            <button class="btn btn-warning">Editar</button>
+          </td>
+          <td>
+            <button class="btn btn-danger">Excluir</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -8,8 +58,7 @@ import BuscaAluno from "./../../components/layout/BuscaAluno";
 export default {
   components: {
     BuscaAluno
-  },
-
+  }
 };
 </script>
 
