079aa85700cdce471bb7a9c87a090b0231a3b546
Leonardo <leonardo@visie.com.br>
2019-06-05 22:01:07 -0300

    Criando modal alterando aluno

--- src/pages/lista-entrada/Lista-alunos.vue
+++ src/pages/lista-entrada/Lista-alunos.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-fluid" id="lista-aluno-tab">
     <h1>Lista de Alunos</h1>
-    <div class="  p-3 mb-2  row" id="lista-alunos-header">
+    <div class="p-3 mb-2 row" id="lista-alunos-header">
       <div class="col-1">Foto</div>
       <div class="col-2">Matricula</div>
       <div class="col-3">Nome</div>
@@ -18,12 +18,121 @@
       <td class="col-3">{{item.resp}}</td>
       <td class="col-2">{{item.nascimento}}</td>
 
-      <td class="col-1">
-        <button class="btn btn-secondary" @click="mostraID(item.id)">Editar</button>
-      </td>
+      <!--  <td class="col-1">
+        <button class="btn btn-secondary" @click="showModal(item.id)">Detalhes</button>
+      </td>-->
+
+      <!-- Button trigger modal -->
+      <button class="btn btn-warning" @click="showModal=true">Editar</button>
+
+      <!-- Modal -->
+      <div
+        class="modal fade"
+        :class="{show: showModal}"
+        :style="{display: showModal ? 'block' : 'none'}"
+      >
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title">Alterar aluno</h1>
+              <button
+                type="button"
+                @click="closeModal"
+                class="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="row">
+               <!--  <div class="form-group col-3">
+                  <input
+                    ref="input"
+                    type="file"
+                    class="d-none"
+                    accept="image/*"
+                    @change="handleFile($event)"
+                  >
+                  <button
+                    @click.prevent="openFileDialog"
+                    type="button"
+                    class="btn btn-outline-secondary"
+                  >Enviar Foto</button>
+                </div> -->
+                <div class="form-group col-9">
+                  <div v-if="form.foto">
+                    {{form.foto.name}}
+                    <button @click="form.foto = ''" class="btn badge badge-light">
+                      <i class="fa fa-trash text-danger"></i>
+                    </button>
+                  </div>
+                </div>
+
+                <div class="form-group col-8">
+                  <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Nome"
+                    v-model="form.nome"
+                    required
+                  >
+                  <small id="emailHelp" class="form-text text-muted">Nome da criança completo</small>
+                </div>
+                <div class="form-group col-4">
+                  <input class="form-control" type="date" v-model="form.nascimento" required>
+                  <small id="emailHelp" class="form-text text-muted">Data de nascimento</small>
+                </div>
+                <div class="form-group col-8">
+                  <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Responsável"
+                    v-model="form.resp"
+                    required
+                  >
+                  <small
+                    id="emailHelp"
+                    class="form-text text-muted"
+                  >Nome dos pais ou responsável pela criança</small>
+                </div>
+                <div class="form-group col-4">
+                  <input
+                    class="form-control"
+                    type="tel"
+                    placeholder="Tel de contato"
+                    v-model="form.tel"
+                    required
+                  >
+                  <small id="emailHelp" class="form-text text-muted">Telefone para contato</small>
+                </div>
+                <div class="form-group col-8">
+                  <select class="custom-select" v-model="form.sala" required>
+                    <option selected>Sala</option>
+                    <option value="Amarela">Amarela</option>
+                    <option value="Verde">Verde</option>
+                    <option value="Azul">Azul</option>
+                  </select>
+                  <small id="emailHelp" class="form-text text-muted">Sala que a criança está</small>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" @click.prevent="closeModal()">Fechar</button>
+              <button type="button" class="btn btn-success">Salvar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="modal-backdrop fade"
+        :style="{display: showModal ? 'block' : 'none'}"
+        :class="{show: showModal}"
+      ></div>
     </div>
 
-    <div>
+    <!-- <div>
       <h1>Atualização de dados</h1>
       <div>
         <input type="text" name="nome" placeholder="nome" v-model="form.nome">
@@ -34,7 +143,7 @@
       <div>
         <input type="Date" name="nome" v-model="form.nascimento">
       </div>
-    </div>
+    </div>-->
   </div>
 </template>
 <script>
@@ -45,6 +154,7 @@ export default {
   },
 
   data: () => ({
+    showModal: false,
     alunoss: [],
     form: {
       nome: "",
@@ -54,6 +164,9 @@ export default {
   }),
 
   methods: {
+    closeModal() {
+      this.showModal = false;
+    },
     async getData() {
       const ref = this.$firebase.database().ref("ListaAlunos");
       ref.on("value", snapshot => {
@@ -110,7 +223,7 @@ img {
   }
 }
 
-#lista-alunos-header{
+#lista-alunos-header {
   font-size: 18pt;
   background-color: var(--gray-soft);
   color: var(--gray);
@@ -119,6 +232,4 @@ img {
 .lista-alunos-item {
   padding: 10px;
 }
-
-
 </style>
