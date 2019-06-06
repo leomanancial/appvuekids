44013c00975aa61c4267850ed6ce86a2402aa1e5
Leonardo <leonardo@visie.com.br>
2019-06-06 13:35:20 -0300

    concluindo modal com os dados recuperados

--- src/pages/lista-entrada/Lista-alunos.vue
+++ src/pages/lista-entrada/Lista-alunos.vue
@@ -17,135 +17,123 @@
       <td class="col-3">{{item.nome}}</td>
       <td class="col-3">{{item.resp}}</td>
       <td class="col-2">{{item.nascimento}}</td>
+      <td>
+        <button
+          class="btn btn-warning btn-sm"
+          @click.prevent="mostraModal(item.foto, item.nome, item.id, item.resp, item.nascimento, item.sala, item.tel )"
+        >Editar</button>
+      </td>
+    </div>
 
-      <!--  <td class="col-1">
-        <button class="btn btn-secondary" @click="showModal(item.id)">Detalhes</button>
-      </td>-->
-
-      <!-- Button trigger modal -->
-      <button class="btn btn-warning" @click="showModal=true">Editar</button>
-
-      <!-- Modal -->
-      <div
-        class="modal fade"
-        :class="{show: showModal}"
-        :style="{display: showModal ? 'block' : 'none'}"
-      >
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title">Alterar aluno</h1>
-              <button
-                type="button"
-                @click="closeModal"
-                class="close"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">&times;</span>
+    <!-- Button trigger modal -->
+    <!-- Modal -->
+    <div
+      class="modal fade"
+      :class="{show: showModal}"
+      :style="{display: showModal ? 'block' : 'none'}"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <div v-if="form.foto">
+              <img v-bind:src="form.foto" class="rounded-circle" id="foto-header">
+              <button @click="form.foto = ''" class="btn badge badge-light">
+                <i class="fa fa-trash text-danger"></i>
               </button>
             </div>
-            <div class="modal-body">
-              <div class="row">
-                <!--  <div class="form-group col-3">
-                  <input
-                    ref="input"
-                    type="file"
-                    class="d-none"
-                    accept="image/*"
-                    @change="handleFile($event)"
-                  >
-                  <button
-                    @click.prevent="openFileDialog"
-                    type="button"
-                    class="btn btn-outline-secondary"
-                  >Enviar Foto</button>
-                </div>-->
-                <div class="form-group col-9">
-                  <div v-if="form.foto">
-                    {{form.foto.name}}
-                    <button @click="form.foto = ''" class="btn badge badge-light">
-                      <i class="fa fa-trash text-danger"></i>
-                    </button>
-                  </div>
-                </div>
+            <h1 class="modal-title">{{this.form.nome}}</h1>
+            <button
+              type="button"
+              @click="closeModal"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+              <!--   <div class="form-group col-3">
+                <input
+                  ref="input"
+                  type="file"
+                  class="d-none"
+                  accept="image/*"
+                  @change="handleFile($event)"
+                >
+                <button
+                  @click="openFileDialog"
+                  type="button"
+                  class="btn btn-outline-secondary"
+                >Enviar Foto</button>
+              </div>-->
+              <div class="form-group col-9"></div>
+              <div class="form-group col-4">
+                <input class="form-control" type="text" v-model="form.id" disabled>
+                <small id="emailHelp" class="form-text text-muted">Matrícula</small>
+              </div>
 
-                <div class="form-group col-4">
-                  <input class="form-control" type="text" v-model="item.id" disabled>
-                  <small id="emailHelp" class="form-text text-muted">Matricula</small>
-                </div>
-                <div class="form-group col-8">
-                  <input
-                    class="form-control"
-                    type="text"
-                    v-model="item.nome"
-                    required
-                  >
-                  <small id="emailHelp" class="form-text text-muted">Nome da criança completo</small>
-                </div>
-                <div class="form-group col-4">
-                  <input class="form-control" type="date" v-model="item.nascimento" required>
-                  <small id="emailHelp" class="form-text text-muted">Data de nascimento</small>
-                </div>
-                <div class="form-group col-8">
-                  <input
-                    class="form-control"
-                    type="text"
-                    v-model="item.resp"
-                    required
-                  >
-                  <small
-                    id="emailHelp"
-                    class="form-text text-muted"
-                  >Nome dos pais ou responsável pela criança</small>
-                </div>
-                <div class="form-group col-4">
-                  <input
-                    class="form-control"
-                    type="tel"
-                    placeholder="Tel de contato"
-                    v-model="item.tel"
-                    required
-                  >
-                  <small id="emailHelp" class="form-text text-muted">Telefone para contato</small>
-                </div>
-                <div class="form-group col-8">
-                  <select class="custom-select" v-model="item.sala" required>
-                    <option selected>Sala</option>
-                    <option value="Amarela">Amarela</option>
-                    <option value="Verde">Verde</option>
-                    <option value="Azul">Azul</option>
-                  </select>
-                  <small id="emailHelp" class="form-text text-muted">Sala que a criança está</small>
-                </div>
+              <div class="form-group col-8">
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="Nome"
+                  v-model="form.nome"
+                  required
+                >
+                <small id="emailHelp" class="form-text text-muted">Nome da criança completo</small>
+              </div>
+              <div class="form-group col-4">
+                <input class="form-control" type="date" v-model="form.nascimento" required>
+                <small id="emailHelp" class="form-text text-muted">Data de nascimento</small>
+              </div>
+              <div class="form-group col-8">
+                <input
+                  class="form-control"
+                  type="text"
+                  placeholder="Responsável"
+                  v-model="form.resp"
+                  required
+                >
+                <small
+                  id="emailHelp"
+                  class="form-text text-muted"
+                >Nome dos pais ou responsável pela criança</small>
+              </div>
+              <div class="form-group col-4">
+                <input
+                  class="form-control"
+                  type="tel"
+                  placeholder="Tel de contato"
+                  v-model="form.tel"
+                  required
+                >
+                <small id="emailHelp" class="form-text text-muted">Telefone para contato</small>
+              </div>
+              <div class="form-group col-8">
+                <select class="custom-select" v-model="form.sala" required>
+                  <option selected>Sala</option>
+                  <option value="Amarela">Amarela</option>
+                  <option value="Verde">Verde</option>
+                  <option value="Azul">Azul</option>
+                </select>
+                <small id="emailHelp" class="form-text text-muted">Sala que a criança está</small>
               </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" @click.prevent="closeModal()">Fechar</button>
-              <button type="button" class="btn btn-success">Salvar</button>
-            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" @click.prevent="closeModal()">Fechar</button>
+            <button class="btn btn-success">Salvar</button>
           </div>
         </div>
       </div>
-      <div
-        class="modal-backdrop fade"
-        :style="{display: showModal ? 'block' : 'none'}"
-        :class="{show: showModal}"
-      ></div>
     </div>
-
-    <!-- <div>
-      <h1>Atualização de dados</h1>
-      <div>
-        <input type="text" name="nome" placeholder="nome" v-model="form.nome">
-      </div>
-      <div>
-        <input type="text" name="nome" placeholder="responsavel" v-model="form.resp">
-      </div>
-      <div>
-        <input type="Date" name="nome" v-model="form.nascimento">
-      </div>
-    </div>-->
+    <div
+      class="modal-backdrop fade"
+      :style="{display: showModal ? 'block' : 'none'}"
+      :class="{show: showModal}"
+    ></div>
   </div>
 </template>
 <script>
@@ -159,13 +147,30 @@ export default {
     showModal: false,
     alunoss: [],
     form: {
+      id: "",
+      foto: "",
       nome: "",
+      nascimento: "",
       resp: "",
-      nascimento: ""
+      tel: "",
+      sala: ""
     }
   }),
 
   methods: {
+    mostraModal(gFoto, gNome, gId, gResp, gNasci, gSala, gTel) {
+      this.showModal = true;
+      this.form.foto = gFoto;
+      this.form.id = gId;
+      this.form.nome = gNome;
+      this.form.resp = gResp;
+      this.form.nascimento = gNasci;
+      this.form.sala = gSala;
+      this.form.tel = gTel;
+
+      console.log(this.form.tel);
+    },
+
     closeModal() {
       this.showModal = false;
     },
@@ -174,32 +179,6 @@ export default {
       ref.on("value", snapshot => {
         const values = snapshot.val();
         this.alunoss = Object.keys(values).map(i => values[i]);
-      });
-    },
-
-    mostraID(iD) {
-      const ref = this.$firebase.database().ref("ListaAlunos");
-      ref.on("value", snapshot => {
-        const values = snapshot.val();
-        this.alunoss = Object.keys(values).map(i => values[i]);
-      });
-
-      this.alunoss.forEach(element => {
-        if (iD == element.id) {
-          ref.child(iD).update(this.form, err => {
-            //Mostra Spinner
-            if (err) {
-              console.error(err);
-            } else {
-              this.$root.$emit("Spinner::hide");
-              alert(
-                "Dados do aluno com a matricula " +
-                  iD +
-                  " alterado com sucesso!"
-              );
-            }
-          });
-        }
       });
     }
   }
@@ -212,7 +191,7 @@ img {
 }
 
 #lista-alunos {
-  font-size: 15pt;
+  font-size: 12pt;
   color: var(--gray);
   transition: 0.45s;
   &.active {
@@ -226,12 +205,16 @@ img {
 }
 
 #lista-alunos-header {
-  font-size: 18pt;
+  font-size: 15pt;
   background-color: var(--gray-soft);
   color: var(--gray);
 }
 
 .lista-alunos-item {
   padding: 10px;
+}
+
+#id {
+  max-width: 70% !important;
 }
 </style>
