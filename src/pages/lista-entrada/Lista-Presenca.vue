1a6a3c7d1d4bce0bfd374aae7783c72062af62ea
Leonardo <leonardo@visie.com.br>
2019-06-17 17:29:54 -0300

    finalizado o salvamento na base de dados da lista de presenca

--- src/pages/lista-entrada/Lista-Presenca.vue
+++ src/pages/lista-entrada/Lista-Presenca.vue
@@ -1,49 +1,52 @@
 <template >
   <div class="container-fluid">
     <h1>Lista de Presença</h1>
-    <div class="row">
-      <div class="form-group col-6">
-        <vue-bootstrap-typeahead
-          prepend="Aluno:"
-          v-model="query"
-          :data="this.aluno"
-          style="max-width:500px"
-          placeholder="Nome do Aluno"
-        >
-          <button
-            type="button"
-            slot="append"
-            class="btn btn-info btn-sm"
-            @click.prevent="addAluno(query)"
-          >Adicionar</button>
-        </vue-bootstrap-typeahead>
+    <form>
+      <div class="row">
+        <div class="form-group col-4">
+          <small id="emailHelp" class="form-text text-muted">Líder do dia</small>
+          <select class="custom-select" v-model="form.liderDia" required>
+            <option value="Fabio e Erica">Fabio e Érica</option>
+            <option value="Fernando e Bete">Fernando e Bete</option>
+            <option value="Janilson e Fabi">Janilson e Fabi</option>
+            <option value="Samuel e Jéssica">Samuel e Jéssica</option>
+            <option value="Vagner e Rita">Vagner e Rita</option>
+          </select>
+        </div>
+        <div class="form-group col-6">
+          <small id="emailHelp" class="form-text text-muted">Digite alguma observação</small>
+          <input class="form-control" v-model="form.obs" type="textbox" placeholder="Observação">
+        </div>
+        <div class="form-group col-2">
+          <small id="emailHelp" class="form-text text-muted">Data da Lista</small>
+          <input class="form-control" type="text" v-model="this.dataLista" disabled>
+        </div>
       </div>
-      <div class="form-group col-4" >
-        <input class="form-control" type="text" placeholder="digite o número do cartão" v-model="form.cartaoL" required>
+      <hr>
+      <div class="row">
+        <div class="form-group col-6">
+          <vue-bootstrap-typeahead
+            prepend="Aluno:"
+            v-model="query"
+            :data="this.aluno"
+            style="max-width:500px"
+            placeholder="Nome do Aluno"
+          ></vue-bootstrap-typeahead>
+        </div>
+        <div class="form-group col-4">
+          <input
+            class="form-control"
+            type="text"
+            placeholder="Digite o número do cartão"
+            v-model="form.cartao"
+            required
+          >
+        </div>
+        <div class="form-group col-2">
+          <button type="button" class="btn btn-info" @click.prevent="addAluno(query)">Adicionar</button>
+        </div>
       </div>
-    </div>
-    <hr>
-    <div class="row">
-      <div class="form-group col-4">
-        <select class="custom-select" v-model="liderDia" required>
-          <option value="Fabio e Erica">Fabio e Érica</option>
-          <option value="Fernando e Bete">Fernando e Bete</option>
-          <option value="Janilson e Fabi">Janilson e Fabi</option>
-          <option value="Samuel e Jéssica">Samuel e Jéssica</option>
-          <option value="Vagner e Rita">Vagner e Rita</option>
-        </select>
-        <small id="emailHelp" class="form-text text-muted">Líder do dia</small>
-      </div>
-      <div class="form-group col-6">
-        <input class="form-control" type="textbox" placeholder="Observação">
-        <small id="emailHelp" class="form-text text-muted">Digite alguma observação</small>
-      </div>
-      <div class="form-group col-2">
-        <input class="form-control" type="text" v-model="this.dataLista" disabled>
-        <small id="emailHelp" class="form-text text-muted">Data da Lista</small>
-      </div>
-    </div>
-
+    </form>
     <table class="table table-hover">
       <thead>
         <tr>
@@ -65,9 +68,7 @@
           <td>{{item.nome}}</td>
           <td>{{item.resp}}</td>
           <td>{{item.sala}}</td>
-          <td class="form-group">
-            <input class="form-control" type="text" placeholder="cartão" v-model="form.cartaoL" disabled>
-          </td>
+          <td>{{form.cartaoL}}</td>
           <td>
             <button class="btn btn-danger">Remover</button>
           </td>
@@ -97,14 +98,16 @@ export default {
       alunoss: [],
       aluno: [],
       form: {
-        fotoL: "",
-        nomeL: "",
-        responsavelL: "",
+        nome: "",
+        responsavel: "",
         idL: "",
-        cartaoL: ""
+        cartao: "",
+        obs: "",
+        liderDia: "",
+        dataListaL: ""
       },
       listaPresenca: [],
-      liderDia: "",
+      refListaPresenca: [],
       dataLista: ""
     };
   },
@@ -114,15 +117,22 @@ export default {
       data.getDate() + "/" + data.getMonth() + "/" + data.getFullYear();
 
     const ref = this.$firebase.database().ref("ListaAlunos");
+
     ref.on("value", snapshot => {
       const values = snapshot.val();
       this.alunoss = Object.keys(values).map(i => values[i]);
 
       for (var s in this.alunoss) {
-        console.log(this.alunoss[s]);
+        //console.log(this.alunoss[s]);
         this.aluno.push(this.alunoss[s].nome);
       }
     });
+    const ref2 = this.$firebase.database().ref("ListaPresenca");
+    ref2.on("value", snapshot => {
+      const values = snapshot.val();
+      this.refListaPresenca = Object.keys(values).map(i => values[i]);
+    });
+    console.log(this.refListaPresenca);
   },
 
   methods: {
@@ -131,13 +141,20 @@ export default {
         if (q == this.alunoss[b].nome) {
           this.listaPresenca.push(this.alunoss[b]);
 
-          console.log(this.listaPresenca);
+          this.form.idL = this.alunoss[b].id;
+          this.form.nome = this.alunoss[b].nome;
+          this.form.responsavel = this.alunoss[b].resp;
+          this.form.dataListaL = this.dataLista;
+
+          //console.log(this.form);
+          //console.log(this.listaPresenca);
         } else {
           console.log("não tem");
         }
       }
       const ref = this.$firebase.database().ref("ListaPresenca");
-      ref.child(this.listaPresenca).update(this.listaPresenca, err => {
+
+      ref.child(this.form.idL).update(this.form, err => {
         if (err) {
           this.$root.$emit("Alerta::show", {
             type: "danger",
@@ -155,7 +172,12 @@ export default {
           });
         }
         this.$root.$emit("Spinner::hide");
+        this.closeModal();
       });
+
+      /* for (var a in this.form) {
+        console.log(this.form[a]);
+      } */
     }
   }
 };
